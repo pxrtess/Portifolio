b971578db46df0aecfd196a67822e29398999a0a
pxrtess <pedrohenports@gmail.com>
2023-06-30 16:52:34 -0300

    LIstagem de projetos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+var projetos = []
+projetos = [{
+    nome: 'Skedoo - TCC',
+    descricao: 'Sistema de gestão voltado a instituições de ensino infantil, desenvolvido em php juntamente com laravel, possui funções como Realtime Chat, cadastro de eventos em calendário, tabela de pagamentos e visualização em dashboard',
+    link: '#',
+    img: '',
+    tipo:'grupo'
+}, {
+  nome: 'Biblioteca',
+  descricao: 'Sistema desenvolvido em React consumindo a API Google Books',
+  link: '#',
+  img: '',
+  tipo:'grupo'
+},{
+    nome: 'BuscaCep',
+    descricao: 'Sistema de busca de cep utilizando a API BrasilAPI',
+    link: '#',
+    img:'',
+    tipo: 'individual'
+},{
+  nome:'Hogwarts',
+  descricao: 'Projeto de fanpage voltado ao universo de Harry Potter, desenvolvido em PHP',
+  link: '#',
+  img: '',
+  tipo:'individual'
+}]
+
+
+function carregarProjetos() {
+    listaEmGrupo = document.getElementById("listaProjetosEmGrupo");
+    listaIndividual =document.getElementById("listaProjetosIndividuais");
+    for (i = 0; i < projetos.length; i++) {
+      projetos[i].tipo == 'individual' ? listaIndividual.innerHTML += `<div class="card"><h3>` + projetos[i].nome + `</h3><p>` + projetos[i].descricao + `</p></div>` : listaEmGrupo.innerHTML += `<div class="card"><h3>` + projetos[i].nome + `</h3><p>` + projetos[i].descricao + `</p></div>`
+    }
+}
 var aberto = false;
 function formContato(){
     var fundo = document.getElementById('fundo')
@@ -20,6 +55,7 @@ function carregando() {
           clearInterval(id);
           document.getElementById("div-carregando").style.display = "none"
           document.getElementById("conteudo").style.display = "block"
+          carregarProjetos();
         } else {
           width++;
           elem.style.width = width + "%";
