12557ddf964a8750e85d5e9f1c28ef9187e003c1
pxrtess <pedrohenports@gmail.com>
2023-06-30 20:27:36 -0300

    Dropdown button

--- js/script.js
+++ js/script.js
@@ -1,67 +1,99 @@
 var projetos = []
 projetos = [{
-    nome: 'Skedoo - TCC',
-    descricao: 'Sistema de gestão voltado a instituições de ensino infantil, desenvolvido em php juntamente com laravel, possui funções como Realtime Chat, cadastro de eventos em calendário, tabela de pagamentos e visualização em dashboard',
-    link: '#',
-    img: '',
-    tipo:'grupo'
+  nome: 'Skedoo - TCC',
+  descricao: 'Sistema de gestão voltado a instituições de ensino infantil, desenvolvido em php juntamente com laravel, possui funções como Realtime Chat, cadastro de eventos em calendário, tabela de pagamentos e visualização em dashboard',
+  link: '#',
+  img: '',
+  tipo: 'grupo'
 }, {
   nome: 'Biblioteca',
   descricao: 'Sistema desenvolvido em React consumindo a API Google Books',
   link: '#',
   img: '',
-  tipo:'grupo'
-},{
-    nome: 'BuscaCep',
-    descricao: 'Sistema de busca de cep utilizando a API BrasilAPI',
-    link: '#',
-    img:'',
-    tipo: 'individual'
-},{
-  nome:'Hogwarts',
+  tipo: 'grupo'
+}, {
+  nome: 'SV Tour',
+  descricao: 'Sistema desenvolvido em React consumindo a API Google Books',
+  link: '#',
+  img: '',
+  tipo: 'grupo'
+}, {
+  nome: 'BuscaCep',
+  descricao: 'Sistema de busca de cep utilizando a API BrasilAPI',
+  link: '#',
+  img: '',
+  tipo: 'individual'
+}, {
+  nome: 'Hogwarts',
   descricao: 'Projeto de fanpage voltado ao universo de Harry Potter, desenvolvido em PHP',
   link: '#',
   img: '',
-  tipo:'individual'
+  tipo: 'individual'
+}, {
+  nome: 'Castlevania',
+  descricao: 'Projeto de fanpage voltado ao universo de castlevania, desenvolvido em PHP',
+  link: '#',
+  img: '',
+  tipo: 'individual'
+}, {
+  nome: 'Pokedex',
+  descricao: 'Projeto de fanpage voltado ao universo de Pokemon, desenvolvido em PHP',
+  link: '#',
+  img: '',
+  tipo: 'individual'
 }]
 
 
 function carregarProjetos() {
-    listaEmGrupo = document.getElementById("listaProjetosEmGrupo");
-    listaIndividual =document.getElementById("listaProjetosIndividuais");
-    for (i = 0; i < projetos.length; i++) {
-      projetos[i].tipo == 'individual' ? listaIndividual.innerHTML += `<div class="card"><h3>` + projetos[i].nome + `</h3><p>` + projetos[i].descricao + `</p></div>` : listaEmGrupo.innerHTML += `<div class="card"><h3>` + projetos[i].nome + `</h3><p>` + projetos[i].descricao + `</p></div>`
-    }
+  listaEmGrupo = document.getElementById("listaProjetosEmGrupo");
+  listaIndividual = document.getElementById("listaProjetosIndividuais");
+  for (i = 0; i < projetos.length; i++) {
+    projetos[i].tipo == 'individual' ? listaIndividual.innerHTML += `<div class="card"><h3>` + projetos[i].nome + `</h3><p>` + projetos[i].descricao + `</p></div>` : listaEmGrupo.innerHTML += `<div class="card"><h3>` + projetos[i].nome + `</h3><p>` + projetos[i].descricao + `</p></div>`
+  }
 }
-var aberto = false;
-function formContato(){
-    var fundo = document.getElementById('fundo')
-    aberto = !aberto;
-    var form = document.getElementById('formContato')
-    var scroll = document.getElementById('body')
-    form.style.display = aberto ? 'block' : 'none' 
-    fundo.style.display = aberto ? 'block' : 'none'
-    scroll.className = aberto ? 'scrolldesabilitado' : ''
+var formAberto = false;
+function formContato() {
+  formAberto = !formAberto;
+  var fundo = document.getElementById('fundo')
+  var form = document.getElementById('formContato')
+  var scroll = document.getElementById('body')
+  form.style.display = formAberto ? 'block' : 'none'
+  fundo.style.display = formAberto ? 'block' : 'none'
+  scroll.className = formAberto ? 'scrolldesabilitado' : ''
+}
+var dropAberto = false;
+function dropdown() {
+  dropAberto = !dropAberto;
+  var scroll = document.getElementById('body')
+  var fundo = document.getElementById('fundo')
+  var dropdown = document.getElementById('dropdown')
+  var btnAbreDrop = document.getElementById('btnAbreDrop')
+  var btnFechaDrop = document.getElementById('btnFechaDrop')
+  dropdown.style.display = dropAberto ? 'block' : 'none'
+  btnAbreDrop.style.display = dropAberto ? 'none' : 'block'
+  btnFechaDrop.style.display = dropAberto ? 'block' : 'none'
+  fundo.style.display = dropAberto ? 'block' : 'none'
+  scroll.className = dropAberto ? 'scrolldesabilitado' : ''
 }
 var percentual = 0;
 function carregando() {
-    if (percentual == 0) {
-      percentual = 1;
-      var elem = document.getElementById("carregando");
-      var width = 1;
-      var id = setInterval(frame, 15);
-      function frame() {
-        if (width >= 100) {
-          clearInterval(id);
-          document.getElementById("fundo").style.display = "none"
-          document.getElementById("div-carregando").style.display = "none"
-          document.getElementById("conteudo").style.display = "block"
-          carregarProjetos();
-        } else {
-          width++;
-          elem.style.width = width + "%";
-        }
+  if (percentual == 0) {
+    percentual = 1;
+    var elem = document.getElementById("carregando");
+    var width = 1;
+    var id = setInterval(frame, 15);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        document.getElementById("fundo").style.display = "none"
+        document.getElementById("div-carregando").style.display = "none"
+        document.getElementById("conteudo").style.display = "block"
+        carregarProjetos();
+      } else {
+        width++;
+        elem.style.width = width + "%";
       }
     }
   }
-  setInterval(carregando, 10);
+}
+setInterval(carregando, 10);
